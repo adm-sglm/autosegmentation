09bb8ca4a31d5c12ac75cd05330249638919cb14
adem_saglam <asaglam84@hotmail.com>
2020-06-08 21:12:25 +0200

    initial

---
+++ Model_predict.py
@@ -0,0 +1,198 @@
+import tensorflow as tf
+import os
+import random
+import numpy as np
+from tqdm import tqdm 
+import matplotlib.pyplot as plt
+import sys
+import glob
+import cv2
+from skimage.io import imread, imshow
+from skimage.transform import resize
+import matplotlib.colors
+
+from medpy.metric.binary import hd, dc
+
+from scipy import stats
+
+import statsmodels.api as sm
+
+import sklearn.metrics as metrics
+from sklearn.metrics import roc_curve, auc 
+
+import scikitplot as skplt
+
+
+
+####################################################################################################################################################################
+
+#np.set_printoptions(threshold=sys.maxsize)
+
+seed = 4558
+np.random.seed = seed
+
+height=384
+width=384
+channels=1
+
+axis =3
+
+"""
+axis here tells the class all of the code below works on ! 
+axis = 0   background  
+axis = 1   right lung
+axis = 2   left lung
+axis = 3   covid-19 affected areas/ diseased areas 
+
+"""
+
+####################################################################################################################################################################
+# importing the coustom metric we used in our model
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+####################################################################################################################################################################
+
+
+#model = tf.keras.models.load_model("U-Net COVID19 Segment model")
+model = tf.keras.models.load_model("model", custom_objects={'dice_coef':dice_coef})  ## loading model
+prediction = np.zeros((height,width,channels),dtype=np.float32) 
+
+img='coronacases_002.ni_z105img.png'
+maskimg='coronacases_002.ni_z105msk.png'
+
+
+prediction= cv2.imread(img,0)
+predictionmask=cv2.imread(maskimg,0)
+#print(np.unique(,return_counts=True,return_index=True))
+predictionmask = (predictionmask == axis).astype(np.bool)
+#print('pred check',predictionmask.max())
+#imshow(predictionmask*255)
+#plt.show()
+#predictionmask=(predictionmask).astype(np.bool)   #######
+
+
+
+norm_image = cv2.normalize(prediction, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)  ## normalising prediction image
+norm_image  = np.expand_dims(norm_image,axis=-1)
+norm_image  = np.expand_dims(norm_image,axis=0)
+#print(norm_image.shape,norm_image.dtype)
+
+
+preds_train= model.predict(norm_image, verbose=1)
+preds_train255=preds_train*255
+# print(preds_train.shape,preds_train.dtype,preds_train.max(),preds_train.min())
+#print(np.unique(preds_train[...,axis],return_counts=True,return_index=True))
+
+#print(np.unique(preds_train[...,axis],return_counts=True,return_index=True))
+#preds_train = (preds_train >= 230).astype(np.uint8)
+#print(np.unique(preds_train[...,axis],return_counts=True,return_index=True))
+
+#hello = (hello>145).astype(np.bool)
+
+####################################################################################################################################################################
+## this function cycles pixel intensity to get the best dice coefficient 
+def getbest_dice(preds_train_func,pred_mask):
+	dice=np.zeros(256,dtype=np.float32)
+	for i in range(0,255):
+		hello=preds_train_func[...,axis].squeeze()
+		hello = (hello>i).astype(np.bool)
+		#ihere+=i
+		dcval= dc(hello,pred_mask)*100
+		#print('here',dcval)
+		#dice= []
+		dice[i]=dcval
+		#dice= int(dice)
+		#data= [dice,i]
+	return dice
+		#if i==255:
+
+####################################################################################################################################################################
+# Calculating Dice coef and Hauf distance 
+
+best_dice= getbest_dice(preds_train255,predictionmask)
+#print(best_dice)
+maxdice= max(best_dice)
+print('dice coefficient',maxdice)    ############################# Val needs to be shown on GUI
+
+#itemindex = np.where(dice==maxdata)
+itemindex = np.argmax(best_dice)
+
+#print('Intensity value at max dice',itemindex)
+
+preds_perfect=(preds_train255>itemindex-1).astype(np.bool)
+preds_perfect = preds_perfect[...,axis].squeeze()
+
+
+haufdist= hd(preds_perfect,predictionmask,voxelspacing=None, connectivity=1)
+print('Hausdorff Distance.',haufdist)   ############################# Val needs to be shown on GUI
+
+####################################################################################################################################################################
+# P-value and A-bland altman plot 
+
+pearson_stats=stats.pearsonr(preds_perfect.flatten(),predictionmask.flatten())
+print('pearson values ','r-value :' ,pearson_stats[0],'p-value :',pearson_stats[1])     ############################# both Vals needs to be shown on GUI
+ 
+
+
+mask_graph=predictionmask*255
+#mask_graph=cv2.imread(maskimg,0)
+#mask_graph=tf.keras.utils.to_categorical(mask_graph, num_classes=4, dtype='uint8')*255
+#mask_graph= mask_graph[...,axis].squeeze()
+
+pred_graph=(preds_train255>itemindex-1).astype(np.uint8)
+pred_graph=pred_graph[...,axis].squeeze()
+
+f, ax = plt.subplots(1, figsize = (8,5))
+sm.graphics.mean_diff_plot(mask_graph.flatten(),pred_graph.flatten(), ax = ax)
+plt.show()              ############################# Plot needs to be shown on GUI
+
+
+
+####################################################################################################################################################################
+# ROC/AUC curve
+
+y_mask = cv2.imread(maskimg,0)
+y_mask = tf.keras.utils.to_categorical(y_mask, num_classes=4, dtype='bool')
+
+y_covid = y_mask[...,axis].squeeze()
+
+#y_probas = np.squeeze(preds_train)
+#y_probas = y_probas[...,axis]
+y_predicted = preds_perfect
+
+ground_truth_labels = y_covid.ravel() # we want to make them into vectors
+score_value= y_predicted.ravel()
+
+fpr, tpr, _ = roc_curve(ground_truth_labels,score_value)
+roc_auc = auc(fpr,tpr)
+fig, ax = plt.subplots(1,1)
+ax.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+ax.plot([0, 1], [0, 1], 'k--')
+ax.set_xlim([0.0, 1.0])
+ax.set_ylim([0.0, 1.05])
+ax.set_xlabel('False Positive rate')
+ax.set_ylabel('True Positive rate ')
+ax.set_title('Receiver operating characteristic for Diseased Areas pixel wise')
+ax.legend(loc="lower right")
+plt.show()                         ############################# Plot needs to be shown on GUI
+
+####################################################################################################################################################################
+# Showing images of the mask and predicted mask
+
+imshow(predictionmask)  ## original loaded mask              ############################# Plot needs to be shown on GUI
+plt.show()
+
+imshow(preds_perfect) ## predicted mask from model           ############################# Plot needs to be shown on GUI
+plt.show()
+
+heatmask=preds_train255[...,axis].squeeze()    # just a heat map to show prediction in a greater detail    ############################# Plot needs to be shown on GUI
+imshow(heatmask)
+plt.show()
+
+####################################################################################################################################################################
+
+
---
+++ app.py
@@ -0,0 +1,177 @@
+from tkinter import *
+import tkinter.filedialog
+from tkinter.ttk import Style
+
+class App:
+
+  def __init__(self, master):
+
+    # All the files in selected path or selected by dialog
+    # self.selected_files = []
+    # self.processed_images = {}
+    # self.processed_masks = {}
+    # self.active_patient_id = 'patient_043'
+    # self.slice_name = 4
+    # self.patient_info = None
+    # self.config_file = None
+    # self.ed_index = '01'
+    # self.es_index = ''
+    # self.data_path = "./dataset/data"
+    # self.images_path = "./dataset/train"
+    self.model = None
+    # self.predprob = 0.5
+    # self.loaded_images = { "ES": None, "ED": None }
+    # # Patient images in nibabel format full path
+    # self.patient_images = {}
+    # # Patient info in a dictionary
+    # self.image_order = 0
+    # GUI related
+    self.master = master
+    self.create_ui()
+    self.create_sidebar()
+    self.load_model()
+
+  def create_ui(self):
+    self.master.geometry("1020x570")
+    self.master.title("MRI Cardiac Segmentation")
+    self.master.style = Style()
+    self.master.style.theme_use("default")
+
+    # self.master.columnconfigure(0, weight=4)
+    # self.master.columnconfigure(1, weight=1)
+
+    # self.leftFrame = Frame(self.master, padx=10, pady=10)
+    # self.leftFrame.grid(row=0, column=0, sticky=N+S+E+W)
+
+    # self.rightFrame = Frame(self.master)
+    # self.rightFrame.columnconfigure(0, weight=1)
+    # self.rightFrame.grid(row=0, column=1, sticky=N+S+E+W)
+
+    # self.menuFrame = Frame(self.rightFrame, relief=RAISED, borderwidth=1, padx=10, pady=10)
+    # self.menuFrame.columnconfigure(0, weight=1)
+    # self.menuFrame.grid(row=0, column=0, sticky=N+S+E+W)
+
+    # self.metricsFrame = Frame(self.rightFrame, relief=RAISED, borderwidth=1)
+    # self.metricsFrame.grid(row=1, column=0, sticky=N+S+E+W)
+
+    # self.imageFrame = Frame(self.leftFrame, relief=RAISED, borderwidth=1, padx=5, pady=5)
+    # self.imageFrame.grid(row=0, column=0)
+
+    # self.imageControlsFrame = Frame(self.leftFrame, relief=RAISED, borderwidth=1, padx=5, pady=5)
+    # self.imageControlsFrame.grid(row=1, column=0)
+
+    # srcLbl = Label(self.imageFrame, text="ED Slice")
+    # srcLbl.grid(row=0, column=0)
+
+    # self.edimg = Label(self.imageFrame)
+    # self.edimg.grid(row=1, column=0, padx=5, pady=5)
+
+    # srcLbl = Label(self.imageFrame, text="ED Mask")
+    # srcLbl.grid(row=0, column=1)
+
+    # self.edmask = Label(self.imageFrame)
+    # self.edmask.grid(row=1, column=1, padx=5, pady=5)
+
+    # srcLbl = Label(self.imageFrame, text="ES Slice")
+    # srcLbl.grid(row=2, column=0)
+
+    # self.esimg = Label(self.imageFrame)
+    # self.esimg.grid(row=3, column=0, padx=5, pady=5)
+
+    # srcLbl = Label(self.imageFrame, text="ES Mask")
+    # srcLbl.grid(row=2, column=1)
+
+    # self.esmask = Label(self.imageFrame)
+    # self.esmask.grid(row=3, column=1, padx=5, pady=5)
+
+    # self.esimg = Label(self.imageFrame)
+    # self.esimg.grid(row=3, column=0, padx=5, pady=5)
+
+    # srcLbl = Label(self.imageFrame, text="ED Predicted Mask")
+    # srcLbl.grid(row=0, column=2)
+
+    # self.edpmask = Label(self.imageFrame)
+    # self.edpmask.grid(row=1, column=2, padx=5, pady=5)
+
+    # srcLbl = Label(self.imageFrame, text="ES Predicted Mask")
+    # srcLbl.grid(row=2, column=2)
+
+    # self.espmask = Label(self.imageFrame)
+    # self.espmask.grid(row=3, column=2, padx=5, pady=5)
+
+    # b = Button(self.imageControlsFrame, text=">", command=self.next_src_image)
+    # b.grid(row=1, column=1, sticky=E)
+    # b = Button(self.imageControlsFrame, text="<", command=self.prev_src_image)
+    # b.grid(row=1, column=0, sticky=W)
+
+  def create_sidebar(self):
+    fselect_button = Button(self.menuFrame, text="Load Data", command=self.open_path_dialog)
+    fselect_button.grid(row=0, sticky=W+E)
+
+    self.exit_button = Button(
+      master=self.menuFrame,
+      text="Exit",
+      command=self.master.quit
+    )
+
+    self.exit_button.grid(row=1, sticky=W+E)
+
+    self.patientNameLbl = Label(self.metricsFrame, text="", anchor="center")
+    self.patientNameLbl.grid(row=5, column=0, padx=10, pady=10)
+
+    tbl_fspace = Label(self.metricsFrame, text="")
+    tbl_fspace.grid(row=0, column=0, padx=5, pady=5)
+
+    lv_ed_lbl = Label(self.metricsFrame, text="LV")
+    lv_ed_lbl.grid(row=1, column=0, padx=5, pady=5)
+
+    dice_lbl = Label(self.metricsFrame, text="Dice coeff.")
+    dice_lbl.grid(row=2, column=0, padx=5, pady=5)
+
+    volume_lbl = Label(self.metricsFrame, text="Volume")
+    volume_lbl.grid(row=3, column=0, padx=5, pady=5)
+
+    volume_lbl = Label(self.metricsFrame, text="EF")
+    volume_lbl.grid(row=4, column=0, padx=5, pady=5)
+
+    ed_tbl_lbl = Label(self.metricsFrame, text="ED")
+    ed_tbl_lbl.grid(row=0, column=1, padx=5, pady=5)
+    es_tbl_lbl = Label(self.metricsFrame, text="ES")
+    es_tbl_lbl.grid(row=0, column=2, padx=5, pady=5)
+
+    self.lv_ed_val = Label(self.metricsFrame, text="")
+    self.lv_ed_val.grid(row=1, column=1, padx=5, pady=5)
+
+    self.lvl_es_val = Label(self.metricsFrame, text="")
+    self.lvl_es_val.grid(row=1, column=2, padx=5, pady=5)
+
+    self.dice_ed = Label(self.metricsFrame, text="")
+    self.dice_ed.grid(row=2, column=1, padx=5, pady=5)
+    self.dice_es = Label(self.metricsFrame, text="")
+    self.dice_es.grid(row=2, column=2, padx=5, pady=5)
+
+    self.vol_ed = Label(self.metricsFrame, text="")
+    self.vol_ed.grid(row=3, column=1, padx=5, pady=5)
+    self.vol_es = Label(self.metricsFrame, text="")
+    self.vol_es.grid(row=3, column=2, padx=5, pady=5)
+
+    self.ef_lbl = Label(self.metricsFrame, text="")
+    self.ef_lbl.grid(row=4, column=1, padx=5, pady=5)
+
+    self.next_patient_btn = Button(self.metricsFrame, text=">", command=self.next_patient)
+    self.next_patient_btn.grid(row=6, column=1)
+
+    self.prev_patient_btn = Button(self.metricsFrame, text="<", command=self.prev_patient)
+    self.prev_patient_btn.grid(row=6, column=0)
+
+  def load_model(self):
+    self.model = tf.keras.models.load_model("model", custom_objects={'dice_coef':dice_coef})  ## loading model
+
+
+
+if __name__ == '__main__':
+  matplotlib.use("Agg")
+  root = Tk()
+  app = App(root)
+
+  root.mainloop()
